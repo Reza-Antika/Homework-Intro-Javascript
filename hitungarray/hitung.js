11735274c1524f7c7a0b88ba125a2e7fd715a879
Reza Antika <88221476+Reza-Antika@users.noreply.github.com>
2021-08-25 19:43:58 +0700

    Add files via upload

---
+++ hitungarray/hitung.js
@@ -0,0 +1,12 @@
+let arr = [6, 10, 4, 6, 9];
+let hasil = 0;
+console.log(arr);
+
+for (i = 0; i < arr.length; i++) {
+   hasil += arr[i];
+}
+console.log(hasil);
+
+if (hasil > 30) {
+   console.log("It`s big!");
+}
